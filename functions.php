9a3976f7ced63f8f97ca4a283e57d17a3f57616b
slateb <slate.ballard@gmail.com>
2015-01-22 21:30:53 -0500

    allow email login

--- functions.php
+++ functions.php
@@ -14,5 +14,33 @@ require_once( get_stylesheet_directory() . '/library/exhipitions.php' );
 require_once( get_stylesheet_directory() . '/library/opportunities.php' );
 require_once( get_stylesheet_directory() . '/library/media-fields.php' );
 
-// Add locations taxonomy for Exhibitions
-// Main Gallery, Project Room, Flat File, Crown St. Window, The Lot, Shop Window, Offsite
+
+// Allow Email Login
+add_filter('authenticate', 'artspace_allow_email_login', 20, 3);
+/**
+ * bainternet_allow_email_login filter to the authenticate filter hook, to fetch a username based on entered email
+ * @param  obj $user
+ * @param  string $username [description]
+ * @param  string $password [description]
+ * @return boolean
+ */
+function artspace_allow_email_login( $user, $username, $password ) {
+    if ( is_email( $username ) ) {
+        $user = get_user_by_email( $username );
+        if ( $user ) $username = $user->user_login;
+    }
+    return wp_authenticate_username_password(null, $username, $password );
+}
+ 
+add_filter( 'gettext', 'addEmailToLogin', 20, 3 );
+/**
+ * addEmailToLogin function to add email address to the username label
+ * @param string $translated_text   translated text
+ * @param string $text              original text
+ * @param string $domain            text domain
+ */
+function addEmailToLogin( $translated_text, $text, $domain ) {
+    if ( "Username" == $translated_text )
+        $translated_text .= __( ' Or Email');
+    return $translated_text;
+}
