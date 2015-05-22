d825c151aaad58275543a6de92565f4b5f7e4118
slateb <slate.ballard@gmail.com>
2015-05-22 09:40:02 -0400

    add artist fields

--- library/artists.php
+++ library/artists.php
@@ -128,3 +128,121 @@ function artist_metaboxes( array $meta_boxes ) {
 
     return $meta_boxes;
 }
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array (
+    'key' => 'group_554cd3570863b',
+    'title' => 'Artist Fields',
+    'fields' => array (
+        array (
+            'key' => 'field_554cd3669db4c',
+            'label' => 'Gallery',
+            'name' => '_artist_gallery',
+            'type' => 'gallery',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array (
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'min' => '',
+            'max' => '',
+            'preview_size' => 'thumbnail',
+            'library' => 'uploadedTo',
+            'min_width' => '',
+            'min_height' => '',
+            'min_size' => '',
+            'max_width' => '',
+            'max_height' => '',
+            'max_size' => '',
+            'mime_types' => '',
+        ),
+    ),
+    'location' => array (
+        array (
+            array (
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'artists',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+));
+
+acf_add_local_field_group(array (
+    'key' => 'group_554cfdb5d1f9c',
+    'title' => 'Artist Options',
+    'fields' => array (
+        array (
+            'key' => 'field_554cfe0319b3e',
+            'label' => 'Artist Header Image',
+            'name' => '_artist_arch_image',
+            'type' => 'image',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array (
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'return_format' => 'url',
+            'preview_size' => 'medium',
+            'library' => 'all',
+            'min_width' => '',
+            'min_height' => '',
+            'min_size' => '',
+            'max_width' => '',
+            'max_height' => '',
+            'max_size' => '',
+            'mime_types' => '',
+        ),
+        array (
+            'key' => 'field_554cfdc9d2a13',
+            'label' => 'Archive Page Text',
+            'name' => '_artist_pagetext',
+            'type' => 'textarea',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array (
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'maxlength' => '',
+            'rows' => '',
+            'new_lines' => 'wpautop',
+            'readonly' => 0,
+            'disabled' => 0,
+        ),
+    ),
+    'location' => array (
+        array (
+            array (
+                'param' => 'options_page',
+                'operator' => '==',
+                'value' => 'acf-options-artist-settings',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+));
+
+endif;
